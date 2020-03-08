d564d7ed5dbc6770d03c5764cced80008e537072
GregHib <gregoryhib@gmail.com>
2020-03-08 15:23:50 +0000

    Send full configuration on startup fixes #15

--- src/main/kotlin/world/gregs/hestia/social/core/LobbyImpl.kt
+++ src/main/kotlin/world/gregs/hestia/social/core/LobbyImpl.kt
@@ -6,9 +6,9 @@ import world.gregs.hestia.social.network.social.encoders.messages.WorldListAll
 class LobbyImpl(private val players: Players, private val transmission: Connection, private val presence: Connection, private val status: OnlineStatus) : Lobby {
 
     override fun connect(player: Player) {
-        //Send worlds
-        player.send(WorldListAll(Server.worlds, configuration = false, status = false))// WorldListUpdate(Server.empty))
         players.add(player)
+        //Send worlds
+        player.send(WorldListAll(Server.worlds, configuration = true, status = true))// WorldListUpdate(Server.empty))
         transmission.connect(player)
         presence.connect(player)
         status.connect(player)
